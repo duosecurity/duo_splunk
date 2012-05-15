c6a9044457b607e11feb8e7aeeca5e8c8052dd48
Jon Oberheide <jon@oberheide.org>
2012-05-15 11:58:58 -0400

    sed and style fixups

--- install.sh
+++ install.sh
@@ -11,24 +11,24 @@ do
         s)  SKEY="$OPTARG";;
         a)  AKEY="$OPTARG";;
         h)  HOST="$OPTARG";;
-        [?]) printf >&2 "Usage: $0 [-d splunk directory] -i duo_ikey -s duo_skey -a duo_akey -h duo_host"
-             printf >&2 "ikey, skey, and host can be found in Duo account's administration panel at admin.duosecurity.com"
-             printf >&2 "see http://www.duosecurity.com/docs/duoweb#generate_an_application_key for instructions on how to generate an akey"
+        [?]) printf >&2 "Usage: $0 [-d splunk directory] -i ikey -s skey -a akey -h host\n"
+             printf >&2 "ikey, skey, and host can be found in Duo account's administration panel at admin.duosecurity.com\n"
+             printf >&2 "see http://www.duosecurity.com/docs/duoweb#generate_an_application_key for instructions on how to generate an akey\n"
              exit 1;;
     esac
 done
 
-if [ -z $IKEY ]; then echo "Missing -i (Duo ikey)"; exit 1; fi
-if [ -z $SKEY ]; then echo "Missing -s (Duo skey)"; exit 1; fi
-if [ -z $AKEY ]; then echo "Missing -a (Duo akey) See http://www.duosecurity.com/docs/duoweb#generate_an_application_key for instructions."; exit 1; fi
-if [ -z $HOST ]; then echo "Missing -h (Duo host)"; exit 1; fi
+if [ -z $IKEY ]; then echo "Missing -i (Duo integration key)"; exit 1; fi
+if [ -z $SKEY ]; then echo "Missing -s (Duo secret key)"; exit 1; fi
+if [ -z $AKEY ]; then echo "Missing -a (Duo application key) See http://www.duosecurity.com/docs/duoweb#generate_an_application_key for instructions."; exit 1; fi
+if [ -z $HOST ]; then echo "Missing -h (Duo API hostname)"; exit 1; fi
 
-echo "installing Duo integration to $SPLUNK"
+echo "Installing Duo integration to $SPLUNK..."
 
 # patch
 patch $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py account.py.diff
 
-echo "copying in Duo integration files"
+echo "Copying in Duo integration files..."
 
 # install duoauth template
 cp duoauth.html $SPLUNK/share/splunk/search_mrsparkle/templates/account/
@@ -40,14 +40,14 @@ cp duo.web.bundled.min.js $SPLUNK/share/splunk/search_mrsparkle/exposed/js/contr
 cp duo_web.py $SPLUNK/lib/python2.7/site-packages/
 
 # configure account.py
-echo "configuring Duo API keys"
-sed -i "" "s/YOUR_DUO_IKEY/$IKEY/g" $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py
-sed -i "" "s/YOUR_DUO_SKEY/$SKEY/g" $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py
-sed -i "" "s/YOUR_DUO_AKEY/$AKEY/g" $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py
-sed -i "" "s/YOUR_DUO_HOST/$HOST/g" $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py
+echo "Configuring Duo API keys..."
+sed -i "s/YOUR_DUO_IKEY/$IKEY/g" $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py
+sed -i "s/YOUR_DUO_SKEY/$SKEY/g" $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py
+sed -i "s/YOUR_DUO_AKEY/$AKEY/g" $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py
+sed -i "s/YOUR_DUO_HOST/$HOST/g" $SPLUNK/lib/python2.7/site-packages/splunk/appserver/mrsparkle/controllers/account.py
 
 # restart splunk web
-echo 'restarting splunkweb'
+echo 'Restarting splunkweb...'
 $SPLUNK/bin/splunk restart splunkweb
 
-echo "done"
+echo "Done installing duo_splunk!"
